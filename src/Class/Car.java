661bcf8dccec2e8c4791e0e9506c2aded4beab74
bee85919 <bee85919@gmail.com>
2023-08-17 11:06:22 +0900

    모두의 자바: 실습

---
+++ src/Class/Car.java
@@ -0,0 +1,7 @@
+package Class;
+
+public class Car {
+    public void print() {
+        System.out.println("Success");
+    }
+}
---
+++ src/Class/CarExam.java
@@ -0,0 +1,8 @@
+package Class;
+
+public class CarExam {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.print();
+    }
+}
