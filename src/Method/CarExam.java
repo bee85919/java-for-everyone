57506537ca12963295fc44e01c3947b4eb9feb07
bee85919 <bee85919@gmail.com>
2023-08-18 11:26:52 +0900

    메서드: 문제

---
+++ src/Method/CarExam.java
@@ -0,0 +1,9 @@
+package Method;
+
+public class CarExam {
+    public static void main(String[] args) {
+        // 아래에서 car에 Car클래스의 인스턴스를 실행하고 run메서드를 사용해보세요.
+        Car car = new Car();
+        car.run();
+    }
+}
